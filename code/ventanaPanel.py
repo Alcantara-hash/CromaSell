64dde58065233fe147262760c3a50160a0fde60d
Alcantara <correoalcantara1@gmail.com>
2023-10-13 19:21:27 -0600

    modified:   code/ventanaPanel.py

--- code/ventanaPanel.py
+++ code/ventanaPanel.py
@@ -1,15 +1,25 @@
 import tkinter as tk
+from baseDeDatos2 import BaseDeDatos
 
 class VentanaPanel(tk.Tk):
 
     def __init__(self):
         super().__init__()
 
-        self.title("Panel")
+        self.title("CromaSell")
         self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}")
+
+        # Widgets de la ventana principal
 
         prueba_label = tk.Label(self, text = "Ventana Panel")
         prueba_label.pack()
+
+        agregarCuentas_button = tk.Button(self, text= "Agregar cuenta", command= self.agregar_cuentas)
+        agregarCuentas_button.pack()
+    
+    def agregar_cuentas(self):
+        pass
+
     
 if __name__ == '__main__':
     app = VentanaPanel()
